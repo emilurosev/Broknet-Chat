c8e238148a9678fdb23ea650032e5014b4d27e7a
emilurosev <urosevemil@gmail.com>
2020-01-24 14:01:43 +0100

    added timestamp to first message

--- src/NewChat/NewChat.js
+++ src/NewChat/NewChat.js
@@ -71,7 +71,8 @@ class NewChat extends React.Component {
     createChat = () => {
         this.props.newChatSubmitFn({
             sendTo: this.state.username,
-            message: this.state.message
+            message: this.state.message,
+            timestamp: Date.now()
         });
     }
 
